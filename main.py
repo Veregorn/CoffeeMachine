e3190a9bbda018cec847779219404e9fd76f935d
Veregorn <rjbarco@gmail.com>
2023-10-24 13:40:55 +0200

    'print_report' function created. Main loop exit condition created

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Data structures
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,4 +31,27 @@ resources = {
     "coffee": 100,
 }
 
-# TODO: Prompt user by asking "What would you like? (espresso/latte/cappuccino): "
+# Global variables
+user_input = ''
+money = 0
+
+# TODO: Create a function that prints the parameters of the coffee machine
+def print_report():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money}")
+
+# TODO: Create a main loop so the machine can serve next customer when current operation has finished
+while True:
+
+    # TODO: Prompt user by asking "What would you like? (espresso/latte/cappuccino): "
+    user_input = input(" What would you like? (espresso/latte/cappuccino): ")
+
+    # We need to have a condition for the App to exit
+    if user_input == 'off':
+        break
+    elif user_input == 'report':
+        print_report()
+
+# TODO: Check resources sufficient
